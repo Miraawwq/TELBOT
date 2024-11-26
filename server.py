d4ce64a3fca5b800e47a8f25cfff44a4f9c19e1b
Miraawwq <vladomore1510@gmail.com>
2024-11-26 22:10:39 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,70 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import random
+import math
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Список всех нарисованных линий
-drawings = []
+# Игровые объекты
+players = {}
+
+# Размер поля
+FIELD_WIDTH = 800
+FIELD_HEIGHT = 600
+
+# Стартовые параметры игрока
+START_RADIUS = 10
+START_X = FIELD_WIDTH // 2
+START_Y = FIELD_HEIGHT // 2
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Когда кто-то рисует, добавляем линии в список и отправляем их всем
-@socketio.on('draw')
-def handle_draw(data):
-    # Добавляем полученные координаты в общий список
-    drawings.append(data)
-    # Отправляем данные о рисовании всем пользователям
-    emit('draw', data, broadcast=True)
+@socketio.on('join_game')
+def on_join_game(data):
+    player_id = data['player_id']
+    # Инициализация нового игрока
+    players[player_id] = {
+        'x': START_X,
+        'y': START_Y,
+        'radius': START_RADIUS
+    }
+    # Отправляем всем игрокам обновленный список игроков
+    emit('game_state', players, broadcast=True)
 
-# Когда пользователь заходит на страницу, отсылаем все нарисованные линии
-@socketio.on('get_drawings')
-def send_drawings():
-    for drawing in drawings:
-        emit('draw', drawing)
+@socketio.on('move_player')
+def on_move_player(data):
+    player_id = data['player_id']
+    move_x = data['move_x']
+    move_y = data['move_y']
+    
+    # Обновляем позицию игрока
+    if player_id in players:
+        players[player_id]['x'] += move_x
+        players[player_id]['y'] += move_y
+        
+        # Поглощение других игроков
+        for other_player_id, other_player in players.items():
+            if other_player_id != player_id:
+                distance = math.sqrt((other_player['x'] - players[player_id]['x'])**2 + 
+                                     (other_player['y'] - players[player_id]['y'])**2)
+                if distance < players[player_id]['radius'] + other_player['radius']:
+                    # Игрок поглощает другого
+                    players[player_id]['radius'] += other_player['radius'] // 2
+                    del players[other_player_id]  # Удаляем поглощенного игрока
+                    break
 
-# Очистка холста для всех пользователей
-@socketio.on('clear')
-def clear_canvas():
-    drawings.clear()
-    emit('clear', broadcast=True)
+    # Отправляем обновленное состояние игры
+    emit('game_state', players, broadcast=True)
+
+@socketio.on('disconnect')
+def on_disconnect():
+    player_id = request.sid
+    if player_id in players:
+        del players[player_id]
+        emit('game_state', players, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, port=5000)
