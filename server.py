14378eba8daf33176de80f61e843c077ec5109d1
Miraawwq <vladomore1510@gmail.com>
2024-11-26 21:42:30 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room, emit, disconnect
 from collections import defaultdict
 
